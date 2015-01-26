f7a66bbab806db9c89b2040fc5dc6882ad34e7f1
Dominic Musgrave <contact@dominicmusgrave.co.uk>
2015-01-26 10:16:15 +0000

    Workaround for issue #18
    Implemented a quick workaround for the issue causing problems when
    loading a texture and compiling in debug mode.
    Using #ifdef to render a circle if debug mode and the texture for any
    other mode

--- Amaze/Amaze/HeroPawn.cpp
+++ Amaze/Amaze/HeroPawn.cpp
@@ -5,12 +5,14 @@ HeroPawn::HeroPawn()
 {
 	WorldX = 0;
 	WorldY = 0;
-
+//if not debug use the texture, this is to address the issue #18 : https://github.com/CarbonBasedDudeform/Amaze/issues/18
+#ifdef NDEBUG
 	_texture = new sf::Texture();
 	_texture->loadFromFile("Textures/hero.png");
 	Size = _texture->getSize().x;
 	_offset = Size / 2;
 	_sprite = new sf::Sprite(*_texture);
+#endif
 }
 
 
@@ -20,6 +22,15 @@ HeroPawn::~HeroPawn()
 
 void HeroPawn::Render(sf::RenderWindow * window)
 {
+#ifdef NDEBUG
 	_sprite->setPosition(WorldX-_offset, WorldY-_offset);
 	window->draw(*_sprite);
+#endif
+
+#if _DEBUG
+	sf::CircleShape circle(10.0f);
+	circle.setFillColor(sf::Color::Green);
+	circle.setPosition(WorldX - _offset, WorldY - _offset);
+	window->draw(circle);
+#endif
 }
